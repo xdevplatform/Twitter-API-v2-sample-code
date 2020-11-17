f49bafde57aa4b2269571b3942ce77bb0fc9de48
John Demos <jdemos@twitter.com>
2020-11-17 16:19:35 -0700

    Adjusts script to use params option on GET request.

--- Recent-Search/recent_search.py
+++ Recent-Search/recent_search.py
@@ -4,26 +4,13 @@ import json
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = os.environ.get("BEARER_TOKEN")
 
+search_url = "https://api.twitter.com/2/tweets/search/recent"
 
-def auth():
-    return os.environ.get("BEARER_TOKEN")
-
-
-def create_url():
-    query = "from:twitterdev -is:retweet"
-    # Tweet fields are adjustable.
-    # Options include:
-    # attachments, author_id, context_annotations,
-    # conversation_id, created_at, entities, geo, id,
-    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-    # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
-        query, tweet_fields
-    )
-    return url
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
 
 
 def create_headers(bearer_token):
@@ -31,8 +18,8 @@ def create_headers(bearer_token):
     return headers
 
 
-def connect_to_endpoint(url, headers):
-    response = requests.request("GET", url, headers=headers)
+def connect_to_endpoint(url, headers, params):
+    response = requests.request("GET", search_url, headers=headers, params=params)
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
@@ -40,10 +27,8 @@ def connect_to_endpoint(url, headers):
 
 
 def main():
-    bearer_token = auth()
-    url = create_url()
     headers = create_headers(bearer_token)
-    json_response = connect_to_endpoint(url, headers)
+    json_response = connect_to_endpoint(search_url, headers, query_params)
     print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
