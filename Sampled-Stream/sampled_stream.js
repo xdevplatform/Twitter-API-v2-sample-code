e3197a341826da4796d36097a8e7480cf90ed969
Aurelia Specker <aspecker@twitter.com>
2021-03-01 22:09:30 +0000

    Fix reconnection logic on timeout and add additional error handling

--- Sampled-Stream/sampled_stream.js
+++ Sampled-Stream/sampled_stream.js
@@ -10,7 +10,7 @@ const token = process.env.BEARER_TOKEN;
 
 const streamURL = 'https://api.twitter.com/2/tweets/sample/stream';
 
-function streamConnect() {
+function streamConnect(retryAttempt) {
 
   const stream = needle.get(streamURL, {
     headers: {
@@ -24,35 +24,38 @@ function streamConnect() {
     try {
       const json = JSON.parse(data);
       console.log(json);
+      // A successful connection resets retry count.
+      retryAttempt = 0;
     } catch (e) {
-      // Keep alive signal received. Do nothing.
+      // Catches error in case of 401 unauthorized error status.
+      if (data.status === 401) {
+        console.log(data);
+        process.exit(1);
+      } else if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+        console.log(data.detail)
+        process.exit(1)
+      } else {
+        console.log(data.detail)
+        // Keep alive signal received. Do nothing.
+      }
     }
-  }).on('error', error => {
-    if (error.code === 'ETIMEDOUT') {
-      stream.emit('timeout');
+  }).on('err', error => {
+    if (error.code !== 'ECONNRESET') {
+      console.log(error.code);
+      process.exit(1);
+    } else {
+      // This reconnection logic will attempt to reconnect when a disconnection is detected.
+      // To avoid rate limits, this logic implements exponential backoff, so the wait time
+      // will increase if the client cannot reconnect to the stream. 
+      setTimeout(() => { 
+        console.warn(`A connection error occurred. Reconnecting...`)
+        streamConnect(++retryAttempt);
+      }, 2 ** retryAttempt);
     }
   });
-
   return stream;
 }
 
 (async () => {
-
-  // Listen to the stream.
-  // This reconnection logic will attempt to reconnect when a disconnection is detected.
-  // To avoid rate limits, this logic implements exponential backoff, so the wait time
-  // will increase if the client cannot reconnect to the stream.
-
-  const sampledStream = streamConnect();
-  let timeout = 0;
-  sampledStream.on('timeout', () => {
-    // Reconnect on error
-    console.warn('A connection error occurred. Reconnecting…');
-    setTimeout(() => {
-      timeout++;
-      streamConnect();
-    }, 2 ** timeout);
-    streamConnect();
-  })
-
+  streamConnect(0)
 })();
