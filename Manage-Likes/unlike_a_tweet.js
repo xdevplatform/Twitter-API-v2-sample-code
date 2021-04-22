69f01a81d992b2b1626df93b6b29a77848c74103
Andy Piper <andypiper@users.noreply.github.com>
2021-04-22 18:09:26 +0100

    Adding sample code for Manage Likes

---
+++ Manage-Likes/unlike_a_tweet.js
@@ -0,0 +1,145 @@
+const got = require('got');
+const crypto = require('crypto');
+const OAuth = require('oauth-1.0a');
+const qs = require('querystring');
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+// The code below sets the consumer key and consumer secret from your environment variables
+// To set environment variables on macOS or Linux, run the export commands below from the terminal:
+// export CONSUMER_KEY='YOUR-KEY'
+// export CONSUMER_SECRET='YOUR-SECRET'
+const consumer_key = process.env.CONSUMER_KEY;
+const consumer_secret = process.env.CONSUMER_SECRET;
+
+
+// Be sure to replace your-user-id with your own user ID or one of an authenticating user
+// You can find a user ID by using the user lookup endpoint
+const id = "your-user-id";
+
+
+// You can replace Tweet ID given with the Tweet ID you wish to like.
+// You can find a Tweet ID by using the Tweet lookup endpoint
+const tweet_id = "1354143047324299264";
+
+const endpointURL = `https://api.twitter.com/2/users/${id}/likes/${tweet_id}`;
+
+
+// this example uses PIN-based OAuth to authorize the user
+const requestTokenURL = 'https://api.twitter.com/oauth/request_token?oauth_callback=oob';
+const authorizeURL = new URL('https://api.twitter.com/oauth/authorize');
+const accessTokenURL = 'https://api.twitter.com/oauth/access_token';
+const oauth = OAuth({
+  consumer: {
+    key: consumer_key,
+    secret: consumer_secret
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
+});
+
+async function input(prompt) {
+  return new Promise(async (resolve, reject) => {
+    readline.question(prompt, (out) => {
+      readline.close();
+      resolve(out);
+    });
+  });
+}
+
+async function requestToken() {
+  const authHeader = oauth.toHeader(oauth.authorize({
+    url: requestTokenURL,
+    method: 'POST'
+  }));
+
+  const req = await got.post(requestTokenURL, {
+    headers: {
+      Authorization: authHeader["Authorization"]
+    }
+  });
+  if (req.body) {
+    return qs.parse(req.body);
+  } else {
+    throw new Error('Cannot get an OAuth request token');
+  }
+}
+
+
+async function accessToken({
+  oauth_token,
+  oauth_token_secret
+}, verifier) {
+  const authHeader = oauth.toHeader(oauth.authorize({
+    url: accessTokenURL,
+    method: 'POST'
+  }));
+  const path = `https://api.twitter.com/oauth/access_token?oauth_verifier=${verifier}&oauth_token=${oauth_token}`
+  const req = await got.post(path, {
+    headers: {
+      Authorization: authHeader["Authorization"]
+    }
+  });
+  if (req.body) {
+    return qs.parse(req.body);
+  } else {
+    throw new Error('Cannot get an OAuth request token');
+  }
+}
+
+
+async function getRequest({
+  oauth_token,
+  oauth_token_secret
+}) {
+
+  const token = {
+    key: oauth_token,
+    secret: oauth_token_secret
+  };
+
+  const authHeader = oauth.toHeader(oauth.authorize({
+    url: endpointURL,
+    method: 'DELETE'
+  }, token));
+
+  const req = await got.delete(endpointURL, {
+    headers: {
+      Authorization: authHeader["Authorization"],
+      'user-agent': "v2UnlikeTweetJS"
+    }
+  });
+
+  if (req.body) {
+    return JSON.parse(req.body);
+  } else {
+    throw new Error('Unsuccessful request');
+  }
+}
+
+
+(async () => {
+  try {
+    // Get request token
+    const oAuthRequestToken = await requestToken();
+    // Get authorization
+    authorizeURL.searchParams.append('oauth_token', oAuthRequestToken.oauth_token);
+    console.log('Please go here and authorize:', authorizeURL.href);
+    const pin = await input('Paste the PIN here: ');
+    // Get the access token
+    const oAuthAccessToken = await accessToken(oAuthRequestToken, pin.trim());
+    // Make the request
+    const response = await getRequest(oAuthAccessToken);
+    console.dir(response, {
+      depth: null
+    });
+  } catch (e) {
+    console.log(e);
+    process.exit(-1);
+  }
+  process.exit();
+})();
