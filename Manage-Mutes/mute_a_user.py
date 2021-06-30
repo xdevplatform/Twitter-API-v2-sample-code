7215c8b945ed223d0b20a6d2752336cfb960088f
Andy Piper <andypiper@users.noreply.github.com>
2021-06-30 18:14:00 +0100

    Adding Manage Mutes samples

---
+++ Manage-Mutes/mute_a_user.py
@@ -0,0 +1,77 @@
+from requests_oauthlib import OAuth1Session
+import os
+import json
+
+# In your terminal please set your environment variables by running the following lines of code.
+# export 'CONSUMER_KEY'='<your_consumer_key>'
+# export 'CONSUMER_SECRET'='<your_consumer_secret>'
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+
+
+# Be sure to replace your-user-id with your own user ID or one of an authenticating user
+# You can find a user ID by using the user lookup endpoint
+id = "your-user-id"
+
+# Be sure to replace id-to-mute with the user id you wish to mute.
+payload = {"target_user_id": "id-to-mute"}
+
+# Get request token
+request_token_url = "https://api.twitter.com/oauth/request_token"
+oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+try:
+    fetch_response = oauth.fetch_request_token(request_token_url)
+except ValueError:
+    print(
+        "There may have been an issue with the consumer_key or consumer_secret you entered."
+    )
+
+resource_owner_key = fetch_response.get("oauth_token")
+resource_owner_secret = fetch_response.get("oauth_token_secret")
+print("Got OAuth token: %s" % resource_owner_key)
+
+# Get authorization
+base_authorization_url = "https://api.twitter.com/oauth/authorize"
+authorization_url = oauth.authorization_url(base_authorization_url)
+print("Please go here and authorize: %s" % authorization_url)
+verifier = input("Paste the PIN here: ")
+
+# Get the access token
+access_token_url = "https://api.twitter.com/oauth/access_token"
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=resource_owner_key,
+    resource_owner_secret=resource_owner_secret,
+    verifier=verifier,
+)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+access_token = oauth_tokens["oauth_token"]
+access_token_secret = oauth_tokens["oauth_token_secret"]
+
+# Make the request
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+# Making the request
+response = oauth.post(
+    "https://api.twitter.com/2/users/{}/muting".format(id), json=payload
+)
+
+if response.status_code != 200:
+    raise Exception(
+        "Request returned an error: {} {}".format(response.status_code, response.text)
+    )
+
+print("Response code: {}".format(response.status_code))
+
+# Saving the response as JSON
+json_response = response.json()
+print(json.dumps(json_response, indent=4, sort_keys=True))
---
+++ Manage-Mutes/unmute_a_user.rb
@@ -0,0 +1,84 @@
+# This script implements the PIN-based OAuth flow to obtain access tokens for a user context request
+# It then makes a User lookup request (by usernames) with OAuth 1.0a authentication (user context)
+
+require 'oauth'
+require 'json'
+require 'typhoeus'
+require 'oauth/request_proxy/typhoeus_request'
+
+# The code below sets the consumer key and secret from your environment variables
+# To set environment variables on Mac OS X, run the export command below from the terminal:
+# export CONSUMER_KEY='YOUR-KEY', CONSUMER_SECRET='YOUR-SECRET'
+consumer_key = ENV["CONSUMER_KEY"]
+consumer_secret = ENV["CONSUMER_SECRET"]
+
+# Be sure to replace your-user-id with your own user ID or one of an authenticating user
+# You can find a user ID by using the user lookup endpoint
+source_user_id = "your-user-id"
+
+# Be sure to add replace id-to-unmute with the user id of the user you wish to unmute.
+# You can find a user ID by using the user lookup endpoint
+target_user_id = "id-to-unmute"
+
+# Returns a user object for one or more users specified by the requested usernames
+user_unmute_url = "https://api.twitter.com/2/users/#{source_user_id}/muting/#{target_user_id}"
+
+consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
+	                                :site => 'https://api.twitter.com',
+	                                :authorize_path => '/oauth/authenticate',
+	                                :debug_output => false)
+
+def get_request_token(consumer)
+
+	request_token = consumer.get_request_token()
+
+  return request_token
+end
+
+def get_user_authorization(request_token)
+	puts "Follow this URL to have a user authorize your app: #{request_token.authorize_url()}"
+	puts "Enter PIN: "
+	pin = gets.strip
+
+  return pin
+end
+
+def obtain_access_token(consumer, request_token, pin)
+	token = request_token.token
+	token_secret = request_token.secret
+	hash = { :oauth_token => token, :oauth_token_secret => token_secret }
+	request_token  = OAuth::RequestToken.from_hash(consumer, hash)
+
+	# Get access token
+	access_token = request_token.get_access_token({:oauth_verifier => pin})
+
+	return access_token
+end
+
+
+def user_unmute(url, oauth_params)
+	options = {
+	    :method => :delete,
+	    headers: {
+	     	"User-Agent": "v2UnmuteUserRuby"
+	    }
+	}
+	request = Typhoeus::Request.new(url, options)
+	oauth_helper = OAuth::Client::Helper.new(request, oauth_params.merge(:request_uri => url))
+	request.options[:headers].merge!({"Authorization" => oauth_helper.header}) # Signs the request
+	response = request.run
+
+	return response
+end
+
+# PIN-based OAuth flow - Step 1
+request_token = get_request_token(consumer)
+# PIN-based OAuth flow - Step 2
+pin = get_user_authorization(request_token)
+# PIN-based OAuth flow - Step 3
+access_token = obtain_access_token(consumer, request_token, pin)
+
+oauth_params = {:consumer => consumer, :token => access_token}
+
+response = user_unmute(user_unmute_url, oauth_params)
+puts response.code, JSON.pretty_generate(JSON.parse(response.body))
