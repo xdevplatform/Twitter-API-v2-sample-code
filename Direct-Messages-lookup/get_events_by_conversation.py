cecb4454272efda828a8e28a48dbfc86fab7122f
@snowman <jimmoffitt@yahoo.com>
2022-11-07 09:41:26 -0700

    Removed abandoned client ID
    Signed-off-by: @snowman <jimmoffitt@yahoo.com>

--- Direct-Messages-lookup/get_events_by_conversation.py
+++ Direct-Messages-lookup/get_events_by_conversation.py
@@ -13,7 +13,7 @@ GET_DMS_EVENTS_URL = "https://api.twitter.com/2/dm_conversations/:dm_conversatio
 #-----------------------------------------------------------------------------------------------------------------------
 # These variables need to be updated to the setting that match how your Twitter App is set-up at
 # https://developer.twitter.com/en/portal/dashboard. These will not change from run-by-run.
-client_id = 'dE5vYmJaa3o5QWRlNEt1T01DZ206MTpjaQ'
+client_id = ''
 #This must match *exactly* the redirect URL specified in the Developer Portal.
 redirect_uri = "https://www.example.com"
 #-----------------------------------------------------------------------------------------------------------------------
